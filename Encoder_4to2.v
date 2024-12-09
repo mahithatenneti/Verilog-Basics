b73e3c6c23cb231e2cd5322a951b703f3c3dd762
mahithatenneti <92201564+mahithatenneti@users.noreply.github.com>
2024-12-09 10:59:45 +0530

    Create Encoder_4to2.v

---
+++ Encoder_4to2.v
@@ -0,0 +1,19 @@
+module priority_encoder( input [3:0]I, output [1:0]O);
+//Data flow Modelling
+assign O[0] = I[1] + I[3];
+assign O[1] = I[2]+ I[3];
+//Gate level Modelling
+/*or (O[0],I[1],I[3]);
+or (O[1],I[2],I[3]);*/
+//behavioural Modelling
+/*always @(*) 
+begin
+case(I)
+4'b0001: O = 2'b00;
+4'b0010: O = 2'b01;
+4'b0100: O = 2'b10;
+4'b1000: O = 2'b11;
+default: O = 2'bXX;
+endcase
+end*/
+endmodule
