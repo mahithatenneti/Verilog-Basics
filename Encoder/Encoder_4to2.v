46c17ae33e433c166a19fcd81b13177a2d0a2615
mahithatenneti <92201564+mahithatenneti@users.noreply.github.com>
2024-12-09 11:31:01 +0530

    Update Encoder_4to2.v

--- Encoder/Encoder_4to2.v
+++ Encoder/Encoder_4to2.v
@@ -5,8 +5,10 @@ module encoder( input [3:0]I, output [1:0]O);
 //Gate level Modelling
 /*or (O[0],I[1],I[3]);
 or (O[1],I[2],I[3]);*/
+endmodule
 //behavioural Modelling
-/*always @(*) 
+/*module encoder( input [3:0]I, output reg [1:0]O);
+always @(*) 
 begin
 case(I)
 4'b0001: O = 2'b00;
@@ -15,5 +17,5 @@ case(I)
 4'b1000: O = 2'b11;
 default: O = 2'bXX;
 endcase
-end*/
-endmodule
+end
+endmodule*/
